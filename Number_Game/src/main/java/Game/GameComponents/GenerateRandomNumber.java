44a247215ca107712e6a2b9163fb08d1db3371ab
Umar Mansoor <i190594@nu.edu.pk>
2023-09-25 13:49:54 +0500

    Number Game Completed

--- Number_Game/src/main/java/Game/GameComponents/GenerateRandomNumber.java
+++ Number_Game/src/main/java/Game/GameComponents/GenerateRandomNumber.java
@@ -14,8 +14,6 @@ public class GenerateRandomNumber implements GenerateRandomNumberInterface{
 		// range will be specific and in between maxNumber and minNumber
 		// like in between 0 - 101 
 		int random_number = rand.nextInt(Min_Max_values.maxNumber + Min_Max_values.minNumber); 
-		System.out.print(random_number);
-		
 		return random_number;
 	}
 
--- Number_Game/src/main/java/Game/GameComponents/NumberGame.java
+++ Number_Game/src/main/java/Game/GameComponents/NumberGame.java
@@ -8,28 +8,59 @@ public class NumberGame {
 	private final static int score_increment = 20;
 	
 	private int Score;
-
+	private final int number_of_attempts = 5;
+	private int used_attempts;
+	
 	public NumberGame(PresentGamePlayIntro presentGamePlayIntro, TakeInputNumber takeInputNumber, GenerateRandomNumber generateRandomNumber){
 		this.gameIntro = presentGamePlayIntro;
 		this.inputNumber = takeInputNumber;
 		this.generateRandomNumber = generateRandomNumber;
+		
 	}
 	
 	public String Start() {
+		Score = 0;
+		this.used_attempts = number_of_attempts;
 		gameIntro.Display_GamePlay_Intro();
 		this.Startround();
 		return EndGame();
 	}
 	
 	private void Startround() {
-		int rand_number = generateRandomNumber.generateRandomNumber();
-		int input_number = inputNumber.getInputNumber();
-		boolean result = chk_guess(rand_number, input_number);
-		ScoreCalculator(result);
 		
+		// loops runs till number of attempts
+		while(used_attempts > 0) {
+			System.out.println("************ Round " + (number_of_attempts - used_attempts + 1) + " Started " + "**************");
+			
+			int rand_number = generateRandomNumber.generateRandomNumber();
+			int input_number = inputNumber.getInputNumber();
+			boolean result = chk_guess(rand_number, input_number);
+			ScoreCalculator(result);
+			--used_attempts;
+		}
+		System.out.println("************ Round Completed **************");
+			
 	}
-	
+	private void chk_guessStatus(int difference) {
+		if(difference < -20 )
+			System.out.println("*********** Too Low **************");
+		else if(difference > 20)
+			System.out.println("*********** Too High *************");
+		else if (difference == 0){
+			System.out.println("*********** Correct **************");
+			System.out.println();
+			System.out.println("*********** You Won **************");
+			System.out.println();
+			used_attempts = 0;
+			Score = 100;
+			}
+		else 
+			System.out.println("*********** Too Close ***********");
+		
+		System.out.println(difference);
+	}
 	private boolean chk_guess(int rand_number, int guess_number) {
+		chk_guessStatus(rand_number - guess_number);
 		if(rand_number - guess_number < 20 && rand_number - guess_number > -20)
 			return true;
 		
@@ -44,11 +75,11 @@ public class NumberGame {
 	}
 	
 	private void ScoreCalculator(boolean result) {
-		Score = getScorePoints(result);
+		Score += getScorePoints(result);
 	}
 	
 	private String EndGame() {
-		String EndingMessage ="****** Game Over ******* \n"
+		String EndingMessage ="************ Game Over ************** \n"
 				+ " Your Score:- " + Score;
 		
 		return EndingMessage;
